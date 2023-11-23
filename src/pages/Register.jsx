4a92a2841bcc4f3607a187f263f2580989656ef4
Shinhyo <shibo6937@gmail.com>
2023-11-23 19:18:43 -0400

    Add register page and styles

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,7 +1,25 @@
 import React from 'react'
+import './styles/register.css'
+import RegisterAccount from '../components/RegisterAccount'
 
 export default function Register() {
+  const activeLink = () => {
+
+  }
+
   return (
-    <div>Register</div>
+    <div id='registerId'>
+      <section id="registerAccountSection">
+        <div className="selectionMenu flexbox">
+          <h3>Selection</h3>
+          <div className="options flexbox">
+            <div>Register</div>
+            <span className="line"></span>
+            <div>Account</div>
+          </div>
+        </div>
+      </section>
+      <RegisterAccount />
+    </div>
   )
 }
