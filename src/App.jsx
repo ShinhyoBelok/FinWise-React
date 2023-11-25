1443f93ae1e998fd77d32aa1bdd39cb7d97940b7
ShinhyoBelok <shibo6937@gmail.com>
2023-11-25 10:24:15 -0400

    Add footer and container styles

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,19 @@ import { Routes, Route } from 'react-router-dom';
 function App() {
 
   return (
-    <div className='flexbox' id='appDiv'>
-      <Navbar />
-      <Routes>
-        <Route exact path="/" element={<Dashboard/>} />
-        <Route exact path="/register" element={<Register />} />
-      </Routes>
+    <div id='appContainer'>
+      <div className='flexbox' id='appDiv'>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Dashboard/>} />
+          <Route exact path="/register" element={<Register />} />
+        </Routes>
+      </div>
+      <footer>
+        <div>ALL RIGHT RESERVED  |</div>
+        <div>|  © <a href="https://github.com/ShinhyoBelok" target='_blank'>Shinhyo BO  |</a></div>
+        <div>|  FinWise</div>
+      </footer>
     </div>
   )
 }
