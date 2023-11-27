b4ff3ff4933b654ba87ca2b77e12bc87862ea4b5
Shinhyo <shibo6937@gmail.com>
2023-11-27 17:04:50 -0400

    Add RegisterAcc component and update active link functionality

---
+++ src/components/RegisterAcc.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function RegisterAcc() {
+  return (
+    <div>RegisterAcc</div>
+  )
+}
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './styles/register.css'
 import RegisterAccount from '../components/RegisterAccount'
+import RegisterAcc from '../components/RegisterAcc';
 
 export default function Register() {
-  const activeLink = () => {
-
+  const [accountState, setaccountState] = useState(false);
+  const activeLink = (e) => {
+    e.preventDefault();
+    const register = document.getElementById('registerOp');
+    const account = document.getElementById('accountOp');
+    
+    if (e.target.id == 'registerOp') {
+      register.classList.add('activeOp')
+      account.classList.remove('activeOp')
+      setaccountState(false);
+    } else if (e.target.id == 'accountOp') {
+      register.classList.remove('activeOp')
+      account.classList.add('activeOp')
+      setaccountState(true);
+    }
   }
 
   return (
@@ -13,13 +27,13 @@ export default function Register() {
         <div className="selectionMenu flexbox">
           <h3>Selection</h3>
           <div className="options flexbox">
-            <div>Register</div>
+            <div onClick={activeLink} id='registerOp' className='activeOp'>Register</div>
             <span className="line"></span>
-            <div>Account</div>
+            <div id='accountOp' onClick={activeLink}>Account</div>
           </div>
         </div>
       </section>
-      <RegisterAccount />
+      {!accountState ? <RegisterAccount /> : <RegisterAcc />}
     </div>
   )
 }
