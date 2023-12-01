b3909cc7674dcfe3b235d624fa1b3439b21d18c8
ShinhyoBelok <shibo6937@gmail.com>
2023-12-01 13:30:03 -0400

    Refactor Register component to use separate forms for registration and account

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,25 +1,14 @@
-import React, { useState } from 'react'
-import './styles/register.css'
-import RegisterAccount from '../components/RegisterAccount'
-import RegisterAcc from '../components/RegisterAcc';
+import React, { useState } from 'react';
+import './styles/register.css';
+import RegisterForm from '../components/RegisterForm';
+import AccountForm from '../components/AccountForm';
 
 export default function Register() {
-  const [accountState, setaccountState] = useState(false);
-  const activeLink = (e) => {
-    e.preventDefault();
-    const register = document.getElementById('registerOp');
-    const account = document.getElementById('accountOp');
-    
-    if (e.target.id == 'registerOp') {
-      register.classList.add('activeOp')
-      account.classList.remove('activeOp')
-      setaccountState(false);
-    } else if (e.target.id == 'accountOp') {
-      register.classList.remove('activeOp')
-      account.classList.add('activeOp')
-      setaccountState(true);
-    }
-  }
+  const [accountState, setAccountState] = useState(false);
+
+  const handleLinkClick = (isAccount) => {
+    setAccountState(isAccount);
+  };
 
   return (
     <div id='registerId'>
@@ -27,13 +16,23 @@ export default function Register() {
         <div className="selectionMenu flexbox">
           <h3>Selection</h3>
           <div className="options flexbox">
-            <div onClick={activeLink} id='registerOp' className='activeOp'>Register</div>
+            <div
+              onClick={() => handleLinkClick(false)}
+              className={` ${!accountState ? 'activeOp' : ''}`}
+            >
+              Register
+            </div>
             <span className="line"></span>
-            <div id='accountOp' onClick={activeLink}>Account</div>
+            <div
+              onClick={() => handleLinkClick(true)}
+              className={` ${accountState ? 'activeOp' : ''}`}
+            >
+              Account
+            </div>
           </div>
         </div>
       </section>
-      {!accountState ? <RegisterAccount /> : <RegisterAcc />}
+      {accountState ? <AccountForm /> : <RegisterForm />}
     </div>
-  )
+  );
 }
