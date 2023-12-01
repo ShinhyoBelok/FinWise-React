5956a2631a3876af77b1d396c332738915297e5d
ShinhyoBelok <shibo6937@gmail.com>
2023-12-01 13:29:58 -0400

    Add FormSelection component and remove unused components

---
+++ src/components/FormSelection.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+export default function FormSelection(props) {
+  const {name, selections, id} = props
+
+  return (
+    <div className="formElement">
+      <div className="formLabel">
+        {id == 'categoryFormId' ?
+          <i class='bx bx-category'></i>
+          :
+          <i className='bx bxs-bank'></i>
+        }
+        {name}
+      </div>
+      <div className="formIn">
+        <select name="cars" id="cars">
+          {selections.map((select) => (
+            <option value={`${select.toLowerCase()}`}>{select}</option>
+          ))}
+        </select> 
+      </div>
+    </div>
+  )
+}
--- src/components/RegisterAcc.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function RegisterAcc() {
-  return (
-    <div>RegisterAcc</div>
-  )
-}
