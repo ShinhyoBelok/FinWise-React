ec852d5a297094c126e0a169663b9060547e0dfa
Shinhyo <shibo6937@gmail.com>
2023-12-04 15:55:21 -0400

    Add new category page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import Navbar from './components/Navbar'
 import Dashboard from './pages/Dashboard'
 import Register from './pages/Register';
+import AddCategory from './pages/AddCategory';
 import { Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
         <Routes>
           <Route exact path="/" element={<Dashboard/>} />
           <Route exact path="/register" element={<Register />} />
+          <Route exact path="/addCategory" element={<AddCategory />} />
         </Routes>
       </div>
       <footer>
