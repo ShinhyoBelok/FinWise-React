a0a7f5f2fe7f79f48c214e89901f45278e437206
Shinhyo <shibo6937@gmail.com>
2023-12-04 15:55:42 -0400

    Add AddCategory component and its styles

---
+++ src/pages/AddCategory.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './styles/addCategory.css'
+import { v4 as uuidv4 } from 'uuid';
+
+export default function AddCategory() {
+  return (
+    <section id="addSection">
+      <span>Add Category</span>
+      <ul className="ulList flexbox">
+        <li>fasdf</li>
+      </ul>
+    </section>
+  )
+}
