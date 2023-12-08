b00135fa2d790c9326092e8631445e0c148604e2
Shinhyo <shibo6937@gmail.com>
2023-12-08 17:33:20 -0400

    Add form for adding categories and display multiple CategoryLi components

--- src/pages/AddCategory.jsx
+++ src/pages/AddCategory.jsx
@@ -1,13 +1,26 @@
 import React from 'react'
 import './styles/addCategory.css'
+import CategoryLi from '../components/CategoryLi';
 import { v4 as uuidv4 } from 'uuid';
 
 export default function AddCategory() {
   return (
     <section id="addSection">
       <span>Add Category</span>
+      <form className="formLabel categotyLi inCategory">
+        <div>
+          <i class='bx bx-plus-circle'></i>
+          <input type="text" placeholder='Category name' className='inputText'/>
+        </div>
+        <button className='submitBtn' type="submit">Add</button>
+      </form>
       <ul className="ulList flexbox">
-        <li>fasdf</li>
+        <CategoryLi />
+        <CategoryLi />
+        <CategoryLi />
+        <CategoryLi />
+        <CategoryLi />
+        <CategoryLi />
       </ul>
     </section>
   )
